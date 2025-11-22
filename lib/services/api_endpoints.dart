c9e010f95dcc3168a911b9d0d28fab1423c6f017
Harshil Shishangiya <harshilshishangiya777@gmail.com>
2025-11-22 21:58:08 +0530

    Add API File And MVVM Folder

---
+++ lib/services/api_endpoints.dart
@@ -0,0 +1,52 @@
+class ApiConstants {
+
+  //Base URL
+  static const String Base_URl = "https://www.prakrutitech.xyz/harshil/";
+
+  //User API
+  static const String View_User = "${Base_URl}ViewUsers.php";
+  static const String Sign_Up= "${Base_URl}SignUp.php";
+  static const String Sign_In = "${Base_URl}SignIn.php";
+  static const String Profile = "${Base_URl}Profile.php";
+  static const String Profile_Update = "${Base_URl}ProfileUpdate.php";
+  static const String Profile_Delete = "${Base_URl}ProfileDelete.php";
+  static const String Change_Password = "${Base_URl}ChangePassword.php";
+
+  //Account API
+  static const String View_Accounts = "${Base_URl}ViewAccounts.php";
+  static const String Add_Account = "${Base_URl}AddAccount.php";
+  static const String Update_Account = "${Base_URl}UpdateAccount.php";
+  static const String Delete_Account = "${Base_URl}DeleteAccount.php";
+
+  //Transaction
+  static const String View_Transactions = "${Base_URl}ViewTransactions.php";
+  static const String Add_Transaction = "${Base_URl}AddTransaction.php";
+  static const String Update_Transaction = "${Base_URl}UpdateTransaction.php";
+  static const String Delete_Transaction = "${Base_URl}DeleteTransaction.php";
+  //static const String Summary_Report_Transaction = "${Base_URl}SummaryReportTransaction.php";
+
+  //Category
+  static const String View_Category = "${Base_URl}ViewCategory.php";
+  static const String Add_Category = "${Base_URl}AddCategory.php";
+  static const String Update_Category = "${Base_URl}UpdateCategory.php";
+  static const String Delete_Category = "${Base_URl}DeleteCategory.php";
+
+  //Expense
+  static const String View_Expenses = "${Base_URl}ViewExpenses.php";
+  static const String Add_Expense = "${Base_URl}AddExpense.php";
+  static const String Update_Expense = "${Base_URl}UpdateExpense.php";
+  static const String Delete_Expense = "${Base_URl}DeleteExpense.php";
+  static const String View_Expense_By_Id = "${Base_URl}ViewExpenseById.php";
+
+  //Income
+  static const String View_Income = "${Base_URl}ViewIncome.php";
+  static const String Add_Income = "${Base_URl}AddIncome.php";
+  static const String Update_Income = "${Base_URl}UpdateIncome.php";
+  static const String Delete_Income = "${Base_URl}DeleteIncome.php";
+  static const String View_Income_By_Id = "${Base_URl}ViewIncomeById.php";
+
+  //Extra
+  static const String Summary_Report = "${Base_URl}SummaryReport.php";
+  static const String Summary_Report_Transaction = "${Base_URl}SummaryReportTransaction.php";
+  static const String Monthly_Summary = "${Base_URl}MonthlySummary.php";
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'api_endpoints.dart';
+
+class ApiService{
+
+  // Users
+  Future<List<User>> fetchUsers() async {
+    final res = await http.get(Uri.parse(ApiConstants.View_User));
+    if (res.statusCode == 200) {
+      final list = json.decode(res.body) as List;
+      return list.map((e) => User.fromJson(e)).toList();
+    }
+    throw Exception('Failed to load users');
+  }
+}
